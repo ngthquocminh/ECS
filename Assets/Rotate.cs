872ee41d3918c2a3842c6d22414541c69ed7134c
Quoc Minh <35948603+ngthquocminh@users.noreply.github.com>
2020-12-29 17:03:14 +0700

    init

---
+++ Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+}
+
+class RotatorSystem : ComponentSystem
+{
+    struct Components
+    {
+        public Transform rotator;
+        public Transform transform;
+    }
+    protected override void OnUpdate()
+    {
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Rendering;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Mesh enemyMesh;
+    [SerializeField] private Material enemyMaterial;
+
+    private EntityManager entityManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 1
+        entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        EntityArchetype archtype = entityManager.CreateArchetype(
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(Rotation),
+            typeof(RenderBounds),
+            typeof(LocalToWorld)
+            ); 
+
+        // 2
+        Entity entity = entityManager.CreateEntity();
+
+        entityManager.AddComponentData(entity, new Translation { Value = new float3(-3f, 0.5f, 5f) });
+        entityManager.AddComponentData(entity, new Rotation { Value = quaternion.EulerXYZ(new float3(0f, 45f, 0f)) });
+
+        entityManager.AddSharedComponentData(entity, new RenderMesh { mesh = enemyMesh, material = enemyMaterial });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
